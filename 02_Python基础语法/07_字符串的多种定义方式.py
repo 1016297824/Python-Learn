75db8defecd6bb6cfe94936a6a1478435ac95007
ZhangKun <1016297824@qq.com>
2025-04-10 16:41:19 +0800

    2025/4/10

---
+++ 02_Python基础语法/07_字符串的多种定义方式.py
@@ -0,0 +1,19 @@
+
+# 单引号定义法，可以包含双引号
+name = '\'"超级可爱小天才"'
+print(type(name))
+print(name)
+
+# 双引号定义法，可以包含单引号
+name = "'霸气可爱小蜜蜂'\""
+print(type(name))
+print(name)
+
+# 三引号定义法
+name =  """
+            \"霸气
+            可爱
+            小州官\"
+        """
+print(type(name))
+print(name)

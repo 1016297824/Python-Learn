2767f5fa2a2cdd11ae9db00d1bf29361ea276902
ZhangKun <1016297824@qq.com>
2025-04-14 16:15:19 +0800

    2025/4/14

---
+++ 03_Python的判断语句/01_布尔类型和比较运算符.py
@@ -0,0 +1,25 @@
+
+
+# 比较运算符
+"""
+    ==,!=,>,<,>=,<=
+"""
+print("10==11结果：", (10 == 11))
+print("10!=11结果：", (10 != 11))
+
+# 字符串比较
+name1 = "霸气可爱小蜜蜂"
+name2 = "霸气可爱小州官"
+print("name1 == name2 结果：",(name1 == name2))
+print("name1 != name2 结果：",(name1 != name2))
+
+
+name3 = "霸气可爱小蜜蜂"
+print("name1 == name3 结果：",(name1 == name3))
+print("name1 != name3 结果：",(name1 != name3))
+
+# test
+name1 = "霸气可爱小蜜蜂"
+name2 = "霸气可爱小州官"
+print("字符串比较：", name1 > name2)
+print("字符串比较：", name1 < name2)

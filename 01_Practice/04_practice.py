dcfdd4a68f309bc6acc02318b80bf0de05fb899a
ZhangKun <1016297824@qq.com>
2025-04-15 21:07:15 +0800

    2025/4/15

--- 01_Practice/04_practice.py
+++ 01_Practice/04_practice.py
@@ -1,16 +1,11 @@
-import random
 
-# 猜数游戏，使用while循环猜 1~100 之间的数
-num = random.randint(1, 100);
-i = 0;
-print("这是一个 1~100 的整数，猜猜这个数是多少：")
-while 1:
-    i += 1;
-    temp = int(input(f"第{i}次猜测："))
-    if temp == num:
-        print(f"恭喜你第{i}次猜对了，这个数就是{num}")
-        break;
-    elif temp < num:
-        print("猜小了")
-    else:
-        print("猜大了")
+# 循环打印 9*9 乘法表
+i = 0
+j = 0
+while i <= 9:
+    i += 1
+    j = i
+    while j <= 9:
+        print(f"{i}*{j}={i*j}", end="\t")
+        j += 1
+    print("\n")
---
+++ 04_Python循环语句/03_while循环嵌套.py
@@ -0,0 +1,11 @@
+
+# while循环嵌套使用
+i = 0
+while i < 10:
+    i += 1
+    j = 0
+    print(f"\n喜欢宝儿的第{i}天")
+    while j < 10:
+        j += 1
+        print(f"今天送宝儿的第{j}朵玫瑰花")
+print(f"\n坚持到第{i}天，亲亲抱抱举高高")

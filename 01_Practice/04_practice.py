cb31e00d21dc9c0900a16ec3fce909c05a3b20b2
ZhangKun <1016297824@qq.com>
2025-04-15 21:27:43 +0800

    2025/4/15

--- 01_Practice/04_practice.py
+++ 01_Practice/04_practice.py
@@ -1,5 +1,5 @@
 
-# 循环打印 9*9 乘法表
+# 循环打印 9*9 乘法表1
 i = 0
 j = 0
 while i <= 9:
@@ -8,4 +8,15 @@ while i <= 9:
     while j <= 9:
         print(f"{i}*{j}={i*j}", end="\t")
         j += 1
+    print("\n")
+
+
+# 循环打印 9*9 乘法表1
+i = 0
+while i < 9:
+    i += 1
+    j = 0
+    while j < i:
+        j += 1
+        print(f"{j}*{i}={j*i}", end="\t")
     print("\n")
